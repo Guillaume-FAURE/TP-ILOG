e9bf83324e4816f4e92f512805ce8fe917f2c38a
Guillaume-FAURE <faureg0717@gmail.com>
2022-02-17 17:01:34 +0100

    startstop

--- TP4-Java-Threads/src/Dialog.java
+++ TP4-Java-Threads/src/Dialog.java
@@ -1,12 +1,30 @@
-public class Dialog implements java.lang.Runnable {
-    @Override
-    public void run(){
-        Thread thrCur = Thread.currentThread();
-        String name = thrCur.getName();
-        System.out.printf("%s : entering Dialog#run\n", name);
-		while (!Thread.interrupted()) {
-			System.out.printf("%s : looping in Dialog#run\n", name);
+public class Dialog { // no longer a Runnable
+	Thread thread;
+	java.lang.Runnable runnable = new java.lang.Runnable() { 
+		@Override
+		public void run() {
+			Thread thrCur = Thread.currentThread();
+			String name = thrCur.getName();
+			System.out.printf("%s entering Dialog#run\n", name);
+			while (!Thread.interrupted()) {
+				System.out.printf("%s looping in Dialog#run\n", name);
+			}
+			System.out.printf("%s leaving Dialog#run", name);
+			thread = null;
 		}
-		// System.out.printf("%s leaving Dialog#run\n", name);
-    }
+	};
+
+	public boolean start() {
+		if (thread == null) {
+			thread = new Thread(runnable);
+			thread.start();
+			return true;
+		}
+		return false;
+	}
+
+	public void stop() {
+		if (thread != null)
+			thread.interrupt();
+	}
 }
--- TP4-Java-Threads/src/TestDialog.java
+++ TP4-Java-Threads/src/TestDialog.java
@@ -1,20 +1,18 @@
 import java.io.IOException;
 
 public class TestDialog {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		Thread thrCur = Thread.currentThread();
 		String name = thrCur.getName();
-		System.out.printf("%s : entering TestDialog#main\n", name);
+		System.out.printf("%s entering TestDialog#main\n", name);
 		Dialog dlg = new Dialog();
-		Thread thrDlg = new Thread(dlg);
-		thrDlg.start();
+		dlg.start();		
 		try {
-			System.in.read(); // wait for enter key
-			thrDlg.interrupt();
-			thrDlg = new Thread(dlg);
-			thrDlg.start();
-		} 
-		catch (IOException e) {
+			System.in.read();
+			dlg.stop(); // stop requested (but not immediately effective)
+			System.in.read();
+			dlg.start();
+		} catch (IOException e) {
 		}
 		System.out.printf("%s leaving TestDialog#main\n", name);
 	}
