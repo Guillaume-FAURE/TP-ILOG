bf734643f58b6c7ae9fa205fd8919a5926207c45
Guillaume-FAURE <faureg0717@gmail.com>
2022-02-17 16:08:37 +0100

    feat(script): add 2loop scriptshot

--- TP4-Java-Threads/src/Dialog.java
+++ TP4-Java-Threads/src/Dialog.java
@@ -3,8 +3,10 @@ public class Dialog implements Runnable {
     public void run(){
         Thread thrCur = Thread.currentThread();
         String name = thrCur.getName();
-        System.out.printf("%s entering Dialog#run\n", name);
-		// ...
-		System.out.printf("%s leaving Dialog#run\n", name);
+        System.out.printf("%s : entering Dialog#run\n", name);
+		while (true) {
+			System.out.printf("%s : looping in Dialog#run\n", name);
+		}
+		// System.out.printf("%s leaving Dialog#run\n", name);
     }
 }
