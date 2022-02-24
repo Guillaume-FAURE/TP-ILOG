83471fcad8b77f7be375f1a83a8c58892982701e
Guillaume-FAURE <faureg0717@gmail.com>
2022-02-24 10:17:08 +0100

    feat(script): add package.json, add test type

--- TP5-TypeScript1/src/main.ts
+++ TP5-TypeScript1/src/main.ts
@@ -7,3 +7,40 @@ function hello(compiler: string){
 }
 
 hello("TypeScript")
+
+let age1: number | undefined
+let name1: string | null = null
+
+enum Color {
+	White, Black, Brown, Red, Orange, Yellow, Green, Blue, Purple, Grey
+}
+let colBackground: Color = Color.Blue
+enum PropKind {
+	Read = 1,
+	Write = 4,
+	Indexed = "indexed",
+}
+let kind: PropKind = PropKind.Read
+
+console.log(kind, PropKind[PropKind.Read]) // 1 Read
+console.log(PropKind.Write, PropKind[PropKind.Write]) // 4 Write
+console.log(PropKind.Indexed) // indexed
+
+type Person={
+    readonly name:string;
+    readonly age:number;
+    job?:string;
+};
+
+let jean: Person = {
+    name : "jean",
+    age : 22,
+}
+
+/**
+ * @description print the name of the person in parameter
+ * @param person person with parameters name and age
+ */
+let printName = (person: Person):void => {console.log(`My name is ${person.name}`)}
+
+printName(jean)
