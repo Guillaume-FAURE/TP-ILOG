d35c5317bbe73952463929d417d2b3bdd40045eb
Guillaume-FAURE <faureg0717@gmail.com>
2022-02-24 09:07:13 +0100

    feat(all): initialisation TP start typescript

---
+++ TP5-TypeScript1/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+/**
+ * @description Print hello arguments
+ * @param compiler arguments, predicted as "TypeScript"
+ */
+function hello(compiler) {
+    console.log("Hello ".concat(compiler));
+}
+hello("TypeScript");
---
+++ TP5-TypeScript1/src/main.ts
@@ -0,0 +1,9 @@
+/**
+ * @description Print hello arguments
+ * @param compiler arguments, predicted as "TypeScript"
+ */
+function hello(compiler: string){
+    console.log(`Hello ${compiler}`);
+}
+
+hello("TypeScript")
