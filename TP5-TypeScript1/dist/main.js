ee8936ab22171409ce252434a6e57187b1db9480
Guillaume-FAURE <faureg0717@gmail.com>
2022-02-24 09:09:11 +0100

    fix(security): suppress dist/**

--- TP5-TypeScript1/dist/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * @description Print hello arguments
- * @param compiler arguments, predicted as "TypeScript"
- */
-function hello(compiler) {
-    console.log("Hello ".concat(compiler));
-}
-hello("TypeScript");
